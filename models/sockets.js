f1b4050529f300980a9f6e1a060a515c94a9fa80
neko-zilla <camilodupont@gmail.com>
2024-01-29 17:12:31 -0500

    se agrega el proyecyo

--- models/sockets.js
+++ models/sockets.js
@@ -10,9 +10,9 @@ class Sockets {
     this.io.on("connection", (socket) => {
         //escuchar el evento mensaje-to-server
         socket.on("mensaje-to-server", (data) => {
-          console.log(data);
+          console.log("mensaje del server",data);
   
-          this.io.emit("mensaje-from-server", data);
+          this.io.emit('mensaje-from-server', data);
         });
         //"socket" es para mandar a un solo usuario conectado el "io" es para todo el mundo
       });
